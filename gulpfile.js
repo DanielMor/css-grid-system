bddaeffecc57eac54833bc00f4938e61488988b3
Daniel Moreira <danielmoreira@sioslife.com>
2017-10-23 12:39:10 +0100

    Disable sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
+//const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
   gulp.src('./src/css-grid-system.scss')
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(sourcemaps.write())
+    //.pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'));
 });
