010ff347ed149fab06ebcf27b2b04318b8d62326
Daniel Moreira <danielmoreira92@gmail.com>
2017-10-27 00:02:38 +0100

    Remove sourcemaps from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-//const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
   gulp.src('./src/css-grid-system.scss')
-    //.pipe(sourcemaps.init())
     .pipe(sass({
+      errLogToConsole: true,
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(autoprefixer({
       cascade: false
     }))
-    //.pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'));
 });
